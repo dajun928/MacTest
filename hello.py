1de91fd395b447954f95892284b3442295d82cb8
dajun928 <1663177102@qq.com>
2019-08-01 00:23:41 +0800

    Revert "test"
    This reverts commit d3d58b41065155f1e8e477b38c958515f7b12ed4.
    
    # Conflicts:
    #	test.py

--- hello.py
+++ hello.py
@@ -1,1 +1,21 @@
-print("hello")
+import requests
+import json
+
+# api路径
+url = "http://127.0.0.1:5000/users"
+
+parms = {
+    'user': 'abc',  # 发送给服务器的内容
+    'pwd': '456'
+}
+
+headers = {
+    'User-agent': 'none/ofyourbusiness',
+    'Spam': 'Eggs'
+}
+
+res = requests.post(url, data=parms, headers=headers)  # 发送请求
+
+text = res.text
+print(json.loads(text))
+
